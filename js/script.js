12d065e968443e6b8dc871b90325cb3a9afe8531
MMax <mr.moshnyatskiy@gmail.com>
2023-05-25 16:50:02 +0300

    Commit 6.2. Refactored next/prev/dots buttons in sliders with regular expressions

--- js/script.js
+++ js/script.js
@@ -379,11 +379,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dots.push(dot);
     }
 
+    function deleteNotDigits(string) {
+        return +string.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+        if (offset == (deleteNotDigits(width) * (slides.length - 1))) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -400,9 +404,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -422,7 +426,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
             
             slideIndex  = slideTo;
-            offset = +width.slice(0, width.length - 2)*(slideTo - 1);
+            offset = deleteNotDigits(width)*(slideTo - 1);
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
